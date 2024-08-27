976a7558ee041253843b218a033085cd8257f259
denisvituk <denvittiuk@gmail.com>
2024-08-27 15:20:31 +0200

    I developed scipt,now it can detect people and title them using database(You can easily add your photo and would suprise how it works)

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 import cv2
+from simple_facerec import SimpleFacerec
 
-body_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+# Encode faces from a folder
+sfr = SimpleFacerec()
+sfr.load_encoding_images("known_faces/")
 
+# Load Camera
 cap = cv2.VideoCapture(0)
 
-if not cap.isOpened():
-    print("Error: Could not open camera.")
-    exit()
 
 while True:
     ret, frame = cap.read()
 
-    if not ret:
-        print("Error: Could not read frame.")
+    # Detect Faces
+    face_locations, face_names = sfr.detect_known_faces(frame)
+    for face_loc, name in zip(face_locations, face_names):
+        y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+
+        cv2.putText(frame, name,(x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
+
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1) & 0xFF  # Mask for only the last 8 bits (to handle non-ASCII inputs)
+
+    if key == 2024 or key == ord('q'):
         break
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    bodies = body_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(50, 50))
-
-    for (x, y, w, h) in bodies:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-    cv2.imshow('People Detection', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
 
 cap.release()
 cv2.destroyAllWindows()
